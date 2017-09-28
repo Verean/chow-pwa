9e6658b0fb4247a831cc050b7813bc85c8eded4d
Verean <verean@gmail.com>
2017-09-27 20:20:26 -0600

    Grid Testing

---
+++ src/gridTest.js
@@ -0,0 +1,25 @@
+//just center aligns row
+<Grid container spacing={0} justify={'center'}>
+    {/* centered row (1 - 12) columns */}
+    <Grid item xs={12}>
+        {/* centered content within row */}
+        <Grid container spacing={0} justify={'center'}>
+            Test
+        </Grid>
+    </Grid>
+    <Grid item xs={12}>
+        <Grid container spacing={0} justify={'center'}>
+            Test
+        </Grid>
+    </Grid>
+    <Grid item xs={12}>
+        <Grid container spacing={0} justify={'center'}>
+            Test
+        </Grid>
+    </Grid>
+    <Grid item xs={12}>
+        <Grid container spacing={0} justify={'center'}>
+            Test
+        </Grid>
+    </Grid>
+</Grid>
