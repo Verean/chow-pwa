6a2a13f982b6359942a81003ab96ec8d31764e11
Verean <verean@gmail.com>
2017-10-05 18:56:07 -0600

    MUI Theme

--- src/index.js
+++ src/index.js
@@ -5,12 +5,31 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import grey from 'material-ui/colors/grey';
+import orange from 'material-ui/colors/orange';
 
 const globalTheme = createMuiTheme({
 	palette: {
-	  primary: grey,
-	  secondary: grey,
-	},
+    primary: grey,
+    secondary: orange,
+  },
+  overrides: {
+    MuiTab: {
+        rootAccent: {
+            color: '#E57373',
+          },
+          rootAccentSelected: {
+            color: 'white',
+          },
+    },
+    MuiTabIndicator: {
+        colorAccent: {
+            backgroundColor: 'white',
+          },
+          colorPrimary: {
+            backgroundColor: 'black',
+          },
+    },
+  },
 });
 
 ReactDOM.render(
